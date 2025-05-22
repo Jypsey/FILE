617d34f8ee0f086a1d3adec4fc53304e2fb1556f
Adish Suresh <adishsuresh3646@gmail.com>
2025-05-22 23:53:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/CodeflixSupport")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
-START_PIC = os.environ.get("START_PIC", "https://envs.sh/sdS.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/sdS.jpg")
+START_PIC = os.environ.get("START_PIC", "https://envs.sh/gCL.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/gCL.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
